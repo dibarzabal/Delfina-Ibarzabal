8defe0c362c75b838ecc88f38219a825509d0f48
dibarzabal <dibarzabal@udesa.edu.ar>
2025-11-07 09:43:37 -0300

    falta

--- clasesSegundaParte/Clase11/simpsons/js/main.js
+++ clasesSegundaParte/Clase11/simpsons/js/main.js
@@ -34,24 +34,40 @@ window.addEventListener('load', function() {
      });
 
     let bart = document.querySelector("#bart");
-    bart.addEventListener("click", function(){
-    })
+    bart.addEventListener("click", function(){})
+
     let lisa = document.querySelector("#lisa");
     lisa.addEventListener("dblclick", function(){})
+
     let marge = document.querySelector("#marge");
     marge.addEventListener("mouseover", function(){})
+
     let homero = document.querySelector("#homero");
     marge.addEventListener("click", function(){})
+
     let maggie = document.querySelector("#maggie");
     maggie.addEventListener("mouseout", function(){})
+
     let milhouse = document.querySelector("#milhouse");
     milhouse.addEventListener("click", function(){})
+
     let burns = document.querySelector("#burns");
+    burns.addEventListener("mouseover", function(){})
+
     let bobPatinio = document.querySelector("#bobPatinio");
+    bobPatinio.addEventListener("dblclick", function(){})
+
     let flanders = document.querySelector("#flanders");
+    flanders.addEventListener("mouseover", function(){})
+
     let duffman = document.querySelector("#duffman");
+    duffman.addEventListener("dblclick", function(){})
+
     let gorgory = document.querySelector("#gorgory");
+    gorgory.addEventListener("mouseover", function(){})
+    
     let nelson = document.querySelector("#nelson")
+    nelson.addEventListener("click", function(){})
  
     /* Capturar pesonajes + Asignarles a cada uno su evento correspondiente, 
     así como también su funcionalidad */
